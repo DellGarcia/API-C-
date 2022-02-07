4038c5f44286c8632e25192f8e264bf40c54e2b8
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2022-02-07 18:30:57 -0300

    Added Xamarin Mobile App

---
+++ XamarinApp/AwesomeApp/AwesomeApp/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AwesomeApp.Pages;
+using XamWebApiClient;
+using AwesomeApp.Services;
+using AwesomeApp.ViewModels;
+
+namespace AwesomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Startup.ConfigureServices();
+            //DependencyService.Register<IUserService, UserService>();
+            //DependencyService.Register<TestViewModel>();
+
+            MainPage = new NavigationPage(new UserRegisterPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
